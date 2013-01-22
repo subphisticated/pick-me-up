ff551b903afce7035f7032b65464cd5f6123ddb5
orangeman <flo@andlabs.de>
2013-01-22 14:29:54 +0100

    LEVEL ONE

--- tests/src/eu/andlabs/fahrgemeinschaft/test/AdventurePuzzleGameSQL.java
+++ tests/src/eu/andlabs/fahrgemeinschaft/test/AdventurePuzzleGameSQL.java
@@ -1,11 +1,15 @@
 package eu.andlabs.fahrgemeinschaft.test;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.test.ProviderTestCase2;
 import android.test.mock.MockContentResolver;
 import eu.andlabs.fahrgemeinschaft.RideProvider;
 
 public class AdventurePuzzleGameSQL extends ProviderTestCase2<RideProvider> {
 
+    private Cursor result;
     private MockContentResolver android;
     private static final String TAG = "CRASH-TEST";
 
@@ -16,9 +20,31 @@ public class AdventurePuzzleGameSQL extends ProviderTestCase2<RideProvider> {
 
 
     /*
-     * Here comes the LEVEL DESIGN:
-     * Not much going on so far...
+     * ### Level 1 ###   Remembering favorite Places  #########################
      */
+    Uri PLACES = Uri.parse("content://eu.andlabs.fahrgemeinschaft/places");
+
+    public void testStorePlaces() {
+
+        Uri pausenhof = android.insert(PLACES, getDummyPlace("Pausenhof"));
+        assertEquals("QUEST:   Append database primary key to pausenhof uri.",
+                pausenhof.getLastPathSegment(), "1");
+
+        result = android.query(PLACES, null, null, null, null);  // 3 Points!
+        assertEquals("QUEST:   database finds one place.", result.getCount(), 1);
+
+        result.moveToFirst();  // 5 Points!
+        assertEquals("QUEST:   is Pausenhof.", result.getString(1), "Pausenhof");
+    }
+
+    private ContentValues getDummyPlace(String name) {
+        ContentValues place = new ContentValues();
+        place.put("name", name);
+        place.put("lat", 23);
+        place.put("lng", 42);
+        return place;
+    }
+
 
 
 
